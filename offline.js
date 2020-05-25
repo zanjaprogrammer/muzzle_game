03b95b2fd1b2dc73cca291036fda79874c7c98ea
Kahfi <33482311+zanjaprogrammer@users.noreply.github.com>
2020-05-25 11:23:35 +0700

    Muzzle_game

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1590380541,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/sprite16-sheet0.png",
+		"images/sprite11-sheet0.png",
+		"images/sprite12-sheet0.png",
+		"images/sprite13-sheet0.png",
+		"images/sprite14-sheet0.png",
+		"images/sprite15-sheet0.png",
+		"images/sprite17-sheet0.png",
+		"images/sprite18-sheet0.png",
+		"media/puzzle-game.ogg"
+	]
+}
